c89ebea51a2b48b1741202dfa6858e58d65be0ad
deepeshvishwa <deepesh.vishwkarma@wdadc.com>
2018-09-17 16:41:24 +0530

    second comment

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
-<?php print "<p>This is Git Testing.<p>";?>
-
 <?php
-for($i=1; $i<=20; $i++){
+
+print "<p>This is Git Testing.<p>";
+
+for($i=1; $i<=10; $i++){
     print "Hello, This is test - $i <br/>";
 }
+
+
+print "This is Test 2";
 ?>
