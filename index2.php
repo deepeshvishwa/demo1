3bebbfd5f5ed2fc36ca24216aa168acabea192a9
deepeshvishwa <deepesh.vishwkarma@wdadc.com>
2018-09-17 17:10:54 +0530

    add one new file and add more 2 files changes

---
+++ index2.php
@@ -0,0 +1,1 @@
+<?php print "<h2>Here is my comman commit into all files and add this file on this time .... 2 ......<h2>";?>
--- phpinfo.php
+++ phpinfo.php
@@ -1,4 +1,5 @@
 <?php print "<h2>Here is my common commitment into both files..........<h2>";?>
+<?php print "<h2>Here is my comman commit into both files.... 2 ......<h2>";?>
 
 
 <?php phpinfo();?>
