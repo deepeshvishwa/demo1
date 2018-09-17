94009aa018783d07b28428f42ce2f9d561f82fca
deepeshvishwa <deepesh.vishwkarma@wdadc.com>
2018-09-17 17:13:53 +0530

    add one file

--- index.php
+++ index.php
@@ -10,4 +10,5 @@ for($i=1; $i<=10; $i++){
 print "This is Test 2";
 
 print "<h2>Here is my comman commit into both files..........<h2>";
+print "<h2>Here is my comman commit into both files.... 2 ......<h2>";
 ?>
