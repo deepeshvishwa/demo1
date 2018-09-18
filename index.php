012b0518e6cd3c3a1c81dbef71b655fd2c7f4d9d
deepeshvishwa <deepesh.vishwkarma@wdadc.com>
2018-09-18 15:49:51 +0530

    add new changes

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 print "<h1>This is index.php page</h1>";
 
-for($i=1; $i<=10; $i++){
+for($i=2; $i<=20; $i++){
     print "<p><b>It is a part of Branch - deep - $i </b></p>";
 }
 ?>
--- index2.php
+++ index2.php
@@ -1,7 +1,25 @@
 <?php
-print "<h1>This is index2.php page</h1>";
 
-for($i=1; $i<=10; $i++){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print "<h1>This is index2.php page and Branch - deep</h1>";
+
+for($i=2; $i<=20; $i++){
     print "<p><b>It is a part of Branch - deep - $i </b></p>";
 }
 
