2a5b44662bf47d0e6c93daeb3022d7695fe6ba90
deepeshvishwa <deepesh.vishwkarma@wdadc.com>
2018-09-18 14:26:49 +0530

    This code from deep branch

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
 <?php
-
-print "<p>This is Git Testing.<p>";
+print "<h1>This is index.php page</h1>";
 
 for($i=1; $i<=10; $i++){
-    print "Hello, This is test - $i <br/>";
+    print "<p><b>It is a part of Branch - deep - $i </b></p>";
 }
-
-
-print "This is Test 2";
-
-print "<h2>Here is my comman commit into both files..........<h2>";
-print "<h2>Here is my comman commit into both files.... 2 ......<h2>";
-
-print "<h2>Here is my comman commit into both files.... 3 ......<h2>";
-print "<h2>Here is my comman commit into both files.... 3 ......<h2>";
 ?>
--- index2.php
+++ index2.php
@@ -1,1 +1,8 @@
-<?php print "<h2>Here is my comman commit into all files and add this file on this time .... 2 ......<h2>";?>
+<?php
+print "<h1>This is index2.php page</h1>";
+
+for($i=1; $i<=10; $i++){
+    print "<p><b>It is a part of Branch - deep - $i </b></p>";
+}
+
+?>
